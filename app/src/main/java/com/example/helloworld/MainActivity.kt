b8c15325b1b88f01eb5b9dca83f532a5c6e6c96d
matthew <matveygamzin322@gmail.com>
2023-03-23 19:51:56 +0300

    optimization

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var str: EditText
+    lateinit var res: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        str = findViewById(R.id.edit_message)
+        res = findViewById(R.id.textField)
+        findViewById<Button>(R.id.send).setOnClickListener {
+            res.text = str.text
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
 }
